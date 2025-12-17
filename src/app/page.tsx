f17f1fdee0025a79fda174e00e0d9045ac29ae88
wmchristopher <79227197+wmchristopher@users.noreply.github.com>
2025-12-16 19:07:51 -0500

    homepage styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <main style={{backgroundImage: 'url("/static/image.png")', backgroundRepeat: "repeat", backgroundSize: "417px 192px"}}
           className="flex-grow p-8">
-      <article className="bg-white/85 border-4 border-white rounded-xl text-center text-lg font-medium h-full flex flex-col justify-center">
-        <h1 className="font-bold text-3xl p-4 text-mallow">
+      <article className="bg-white/85 border-4 border-white rounded-xl text-center text-md font-medium h-full flex flex-col justify-center">
+        <h1 className="font-bold text-4xl p-4 mb-5 text-mallow">
           Welcome!
         </h1>
         <p>
